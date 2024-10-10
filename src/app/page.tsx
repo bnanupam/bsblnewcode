7079bc145cd4586c07735a832186d64ed33ecb7c
Anupam <158136988+bnanupam@users.noreply.github.com>
2024-10-10 08:25:43 +0530

    all updated code till yesterday

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "@/components/Landing/HeroSection";
+import Services from "@/components/Landing/Services";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Services />
+    </>
+  );
+}
---
+++ src/components/Landing/Services.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Card } from "./Card";
+
+const Services = () => {
+  return (
+    <>
+      <div className="">
+        <h1 className=" text-primary"> services</h1>
+        <Card />
+      </div>
+    </>
+  );
+};
+
+export default Services;
